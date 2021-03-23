479665ddd4533d1c2ba1add90d7641df13aca56a
spi8823 <spi8823@Katori.localdomain>
2021-03-24 06:00:48 +0900

    PathのDefineクラスを作成

---
+++ Data/Jinja/Define.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wadatsumi.Data.Jinja
+{
+    public static class PathDefine
+    {
+        public const string JinjaResourceRootPath = "wwwroot/jinja_resource/";
+        public const string GoshuinRootPath = JinjaResourceRootPath + "goshuin/";
+        public const string JinjaDBFilePath = JinjaResourceRootPath + "jinja.db";
+        public const string ImagesFolderPath = JinjaResourceRootPath + "images/";
+
+        public static string GetGoshuinTraveloguePath(int id) => $"{GoshuinRootPath}{id}/travelogue.html";
+    }
+}
